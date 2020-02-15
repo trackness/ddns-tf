9d59f1ca57a008b9ec4cfd28eb4ccc47417b36e1
James Cunningham <james.cunningham@bbc.co.uk>
2020-02-15 23:24:30 +0000

    Added flow for hosted_zone inputs

--- modules/main.tf
+++ modules/main.tf
@@ -1,3 +1,13 @@
+variable "hosted_zones" {
+
+}
+
 module "get_ip" {
   source = "./get_ip"
+}
+
+module "set_ip" {
+  source        = ".//set_ip"
+  home_ip       = module.get_ip.home_ip
+  route_53_urls = var.hosted_zones
 }
