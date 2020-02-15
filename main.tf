287ede630d4ebb0c13973dbb01c77a001836ee80
James Cunningham <james.cunningham@bbc.co.uk>
2020-02-15 23:23:57 +0000

    Added aws provider

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
+variable "region" { default = "eu-west-2" }
+
 provider "http" { version = "1.1" }
+provider "aws" {
+  version = "2.48"
+  region = var.region
+}
 
 module "ddns" {
   source = "./modules"
