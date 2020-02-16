15b3791d13899e2100436d5508809045dc45e65f
James Cunningham <james.cunningham@bbc.co.uk>
2020-02-16 00:39:40 +0000

    Added protection and refinement for input config.yaml

--- main.tf
+++ main.tf
@@ -1,4 +1,7 @@
-variable "region" { default = "eu-west-2" }
+variable "region" {
+  description = "The AWS region in which the profile credentials are valid."
+  default = "eu-west-2"
+}
 
 provider "http" { version = "1.1" }
 provider "aws" {
@@ -7,16 +10,18 @@ provider "aws" {
 }
 
 module "ddns" {
+  description = "A home DDNS service for Route53 domains powered by Terraform."
   source = "./modules"
   hosted_zones = local.hosted_zones
 }
 
 locals {
-  hosted_zones_map = var.yaml_config != "" ? yamldecode(file("./${var.yaml_config}")) : []
+  hosted_zones_map = var.yaml_config != "" ? yamldecode(file("${path.root}/${var.yaml_config}")) : []
   hosted_zones = [for hosted_zone in local.hosted_zones_map : hosted_zone.url]
 }
 
 variable "yaml_config" {
+  description = "Location of the yaml file containing the URLS to be redirected relative to the root module."
   type = string
   default = ""
 }
