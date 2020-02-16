ed14e06d2962771ebc325ee97c96e8d13e596df0
James Cunningham <james.cunningham@bbc.co.uk>
2020-02-16 00:11:12 +0000

    Completed flow from config file to set_ip module

--- main.tf
+++ main.tf
@@ -8,4 +8,15 @@ provider "aws" {
 
 module "ddns" {
   source = "./modules"
+  hosted_zones = local.hosted_zones
+}
+
+locals {
+  hosted_zones_map = var.yaml_config != "" ? yamldecode(file("./${var.yaml_config}")) : []
+  hosted_zones = [for hosted_zone in local.hosted_zones_map : hosted_zone.url]
+}
+
+variable "yaml_config" {
+  type = string
+  default = ""
 }
