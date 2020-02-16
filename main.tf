5daba336ce152e4db75d7fc1627ca805b3e2f57e
James Cunningham <james.cunningham@bbc.co.uk>
2020-02-16 00:53:17 +0000

    Refactor to remove unneccessary module layer

--- main.tf
+++ main.tf
@@ -1,18 +1,19 @@
-variable "region" {
-  description = "The AWS region in which the profile credentials are valid."
-  default = "eu-west-2"
-}
-
 provider "http" { version = "1.1" }
+
 provider "aws" {
   version = "2.48"
   region = var.region
 }
 
-module "ddns" {
-  description = "A home DDNS service for Route53 domains powered by Terraform."
-  source = "./modules"
-  hosted_zones = local.hosted_zones
+variable "region" {
+  description = "The AWS region in which the profile credentials are valid."
+  default = "eu-west-2"
+}
+
+variable "yaml_config" {
+  description = "Location of the yaml file containing the URLS to be redirected relative to the root module."
+  type = string
+  default = ""
 }
 
 locals {
@@ -20,8 +21,12 @@ locals {
   hosted_zones = [for hosted_zone in local.hosted_zones_map : hosted_zone.url]
 }
 
-variable "yaml_config" {
-  description = "Location of the yaml file containing the URLS to be redirected relative to the root module."
-  type = string
-  default = ""
+module "get_ip" {
+  source = "./get_ip"
+}
+
+module "set_ip" {
+  source        = "./set_ip"
+  home_ip       = module.get_ip.home_ip
+  route_53_urls = local.hosted_zones
 }
